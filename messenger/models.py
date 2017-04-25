9f0aad847f8ae453b734e6d3be59b3ffd6be5ec2
Andrew Gilchrist <andrew.williams.gilchrist@aggiemail.usu.edu>
2017-04-25 17:48:17 +0000

    Basic layout of project and basic implementation of model, and index html page

--- messenger/models.py
+++ messenger/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Message(models.Model):
+    encryptedText = models.CharField(max_length = 10000)
+    encryptionKey = models.CharField(max_length = 10)
+    destination = models.EmailField(max_length = 256)
+    
+    def __str__(self):
+        return self.destination
+    
--- messenger/views.py
+++ messenger/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Message
 
 # Create your views here.
+def index(request):
+    return render(request, "messenger/index.html")
