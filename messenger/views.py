5937c57a1890f710c9361b19fbe6617929204076
Andrew Gilchrist <andrew.williams.gilchrist@aggiemail.usu.edu>
2017-04-26 21:52:44 +0000

    Submitting form correctly displays submission page

--- messenger/views.py
+++ messenger/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Message
+from models import Message
 
 # Create your views here.
 def index(request):
     return render(request, "messenger/index.html")
+    
+def submit(request):
+    return render(request, "messenger/submit.html")
